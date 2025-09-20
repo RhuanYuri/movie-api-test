50fb4866ae2d4f2bb97ba7da89b0b7b32402171b
RhuanYuri <rhuanyuri12@gmail.com>
2025-09-20 18:40:41 -0300

    fix: remover valores padrão da configuração do banco de dados e garantir que a entidade do usuário esteja anotada corretamente

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,12 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'cinemais',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
+  autoLoadEntities: true,
   logging: true,
 });
