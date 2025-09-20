0c8a8cc271984486b05b16fcc345297ce92aeacf
RhuanYuri <rhuanyuri12@gmail.com>
2025-09-20 12:09:22 -0300

    chore: adicionar conexão com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+dotenvConfig();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(databaseConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
