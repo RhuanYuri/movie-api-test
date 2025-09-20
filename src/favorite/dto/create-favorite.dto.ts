7e81de1cf834aab99090cbc5371abf407ea94ea1
RhuanYuri <rhuanyuri12@gmail.com>
2025-09-20 19:12:51 -0300

    feat: adicionar DTOs para Favorite

--- src/favorite/dto/create-favorite.dto.ts
+++ src/favorite/dto/create-favorite.dto.ts
@@ -1,1 +1,14 @@
-export class CreateFavoriteDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+export class CreateFavoriteDto {
+  @ApiProperty({ description: 'ID do usuário', example: 'uuid-do-usuario' })
+  @IsUUID()
+  @IsNotEmpty()
+  userId: string;
+
+  @ApiProperty({ description: 'ID da mídia', example: 'uuid-da-midia' })
+  @IsUUID()
+  @IsNotEmpty()
+  mediaId: string;
+}
--- src/favorite/dto/update-favorite.dto.ts
+++ src/favorite/dto/update-favorite.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateFavoriteDto } from './create-favorite.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsUUID, IsOptional } from 'class-validator';
 
-export class UpdateFavoriteDto extends PartialType(CreateFavoriteDto) {}
+export class UpdateFavoriteDto extends PartialType(CreateFavoriteDto) {
+  @ApiPropertyOptional({ description: 'ID do usuário' })
+  @IsUUID()
+  @IsOptional()
+  userId?: string;
+
+  @ApiPropertyOptional({ description: 'ID da mídia' })
+  @IsUUID()
+  @IsOptional()
+  mediaId?: string;
+}
